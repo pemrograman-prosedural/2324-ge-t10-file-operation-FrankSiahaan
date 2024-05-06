0f1f6c522f11ca1679eed10204ba470bb6e5aa56
FrankSiahaan <frankniroysiahaan1@gmail.com>
2024-05-06 20:57:57 +0700

    t10

--- t10_01.c
+++ t10_01.c
@@ -7,135 +7,99 @@
 
 int main(int _argc, char **_argv)
 {
-    struct student_t *students = malloc(50 * sizeof(struct student_t));
-    struct dorm_t *dorms = malloc(50 * sizeof(struct dorm_t));
-    char cmd[255];
-    char id[12];
-    char name[40];
-    char year[5];
-    char names[100];
+    struct student_t *students = malloc(12 * sizeof(struct student_t));
+    struct dorm_t *dorms = malloc(12 * sizeof(struct dorm_t));
+    char buffer[100];
     char *token;
-    unsigned short capacity;
-    int i = 0; 
+    char name[255];
+    char id[255];
+    char year[255];
+    int i = 0;
     int j = 0;
-    int student_index = 0;
-    int dorm_index = 0;
-    char buff[255];
-    
-    FILE *file = fopen("student-repository.txt", "r");
-    if (fgets(buff, sizeof(buff), file) == NULL )
-    {
-        fclose(file);
-    }
+    char named[255];
+    int capacity;
+    char cmd[255];
     
 
+    FILE *filestd;
+    filestd = fopen("./storage/student-repository.txt", "r");
+    while (fgets(buffer, 100 , filestd) != NULL)
+    {
+        token = strtok(buffer, "|");
+        strcpy(id, token);
+
+        token = strtok(NULL, "|");
+        strcpy(name, token);
+
+        token = strtok(NULL, "|");
+        strcpy(year, token);
+
+        token = strtok(NULL, "|");
+        if (strcmp(token, "male") == 0)
+        {
+            students[i] = create_student(id, name, year, GENDER_MALE);
+        } else if (strcmp(token, "female") == 0)
+        {
+            students[i] = create_student(id, name, year, GENDER_FEMALE);
+        }
+        i++;
+    }
+    fclose(filestd);
+
+    FILE *filedrm;
+    filedrm = fopen("./storage/dorm-repository.txt", "r");
+    while (fgets(buffer, 100 , filedrm) != NULL)
+    {
+        token = strtok(buffer, "|");
+        strcpy(named, token);
+
+        token = strtok(NULL, "|");
+        capacity = atoi(token);
+
+        token = strtok(NULL, "|");
+        if (strcmp(token, "male") == 0)
+        {
+            dorms[j] = create_dorm(named, capacity, GENDER_MALE);
+        } else if (strcmp(token, "female") == 0)
+        {
+            dorms[j] = create_dorm(named, capacity, GENDER_FEMALE);
+        }
+        j++;
+    }
+    fclose(filedrm);
+    
     while (1)
     {
-        fgets(cmd, sizeof(cmd), stdin);
+        fgets(cmd, sizeof(cmd), stdin);    
       
         if (cmd[strlen(cmd) - 1] == '\n' || cmd[strlen(cmd) - 1] == '\r')
         {
             cmd[strlen(cmd) - 1] = '\0';
         }
-
-        if (strcmp(cmd, "---") == 0)
+        if(strcmp(cmd, "---") == 0)
         {
             break;
         }
         else
         {
-            strcpy(token, buff);
-            token = strtok(cmd, "#");
-            if (strcmp(token, "student-add") == 0)
+            if (strcmp(cmd, "dorm-print-all-detail") == 0)
             {
-                token = strtok(NULL, "#");
-                strcpy(id, token);
-
-                token = strtok(NULL, "#");
-                strcpy(name, token);
-
-                token = strtok(NULL, "#");
-                strcpy(year, token);
-
-                token = strtok(NULL, "#");
-                if (strcmp(token, "male") == 0)
-                {
-                    students[i] = create_student(id, name, year, GENDER_MALE);
-                }
-                else if (strcmp(token, "female") == 0)
-                {
-                    students[i] = create_student(id, name, year, GENDER_FEMALE);
-                }
-                i++;
-            }
-            else if (strcmp(token, "student-print-all") == 0)
+                print_dorm_detail(dorms, j);
+            }else if(strcmp(cmd, "student-print-all-detail") == 0) 
             {
-                void (*pf) (struct student_t *student, int y);
-                pf = print_student;
-                pf(students, i);
-            } 
-            else if (strcmp(token, "student-print-all-detail") == 0) {
-                void (*pf) (struct student_t *student, int y);
-                pf = print_student_detail;
-                pf(students, i);
+                print_student_detail(students, i);
+            }else if (strcmp(cmd, "student-print-all") == 0)
+            {
+                print_student(students, i);
+            }else if(strcmp(cmd, "dorm-print-all") == 0) {
+                print_dorm(dorms, j);
             }
-            else if (strcmp(token, "dorm-print-all-detail") == 0) {
-                void (*pf) (struct dorm_t *dorm, int y);
-                pf = print_dorm_detail;
-                pf(dorms, j);
-            }
-            else if (strcmp(token, "dorm-add") == 0) {
-                token = strtok(NULL, "#");
-                strcpy(name, token);
-
-                token = strtok(NULL, "#");
-                capacity = atoi(token);
-
-                token = strtok(NULL, "#");
-                 if (strcmp(token, "male") == 0)
-                {
-                    dorms[j] = create_dorm(name, capacity, GENDER_MALE);
-                }
-                else if (strcmp(token, "female") == 0)
-                {
-                    dorms[j] = create_dorm(name, capacity, GENDER_FEMALE);
-                }
-                j++;
-            }
-            else if (strcmp(token, "dorm-print-all") == 0) {
-                void (*pf) (struct dorm_t *dorm, int y);
-                pf = print_dorm;
-                pf(dorms, j);
-            }
-            else if (strcmp(token, "assign-student") == 0) {
-                token = strtok(NULL, "#");
-                strcpy(id, token);
-
-                token = strtok(NULL, "#");
-                strcpy(names, token);
-
-                for (int s = 0; s < i; ++s)
-                {
-                    if (strcmp(students[s].id, id) == 0)
-                    {
-                        student_index = s;      
-                        break; 
-                    }
-                }   
-                for (int d = 0; d < i; ++d)
-                {
-                    if (strcmp(dorms[d].name, names) == 0)
-                    {
-                        dorm_index = d;    
-                        break;   
-                    }
-                }   
-                assign_student(&students[student_index], &dorms[dorm_index], id, names);
-            }
+            
         }
-    }
+        }
 
     free(students);
     free(dorms);
+
     return 0;
 }
